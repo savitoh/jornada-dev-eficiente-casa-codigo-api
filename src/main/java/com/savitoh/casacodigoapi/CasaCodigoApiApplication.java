6706068f89cb4a89ab996393932734ae0c8a184e
savitoh <savioraires10@gmail.com>
2020-01-20 22:47:23 -0300

    Setup

---
+++ src/main/java/com/savitoh/casacodigoapi/CasaCodigoApiApplication.java
@@ -0,0 +1,13 @@
+package com.savitoh.casacodigoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaCodigoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaCodigoApiApplication.class, args);
+	}
+
+}
